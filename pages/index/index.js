9ab6635602b5af4fc7bcf54fc0da7afedb3ca033
inzaghihao <wangyipippo@163.com>
2017-12-05 11:08:42 +0800

    v1.0

---
+++ pages/index/index.js
@@ -0,0 +1,15 @@
+//index.js
+Page({
+  data: {
+    
+  },
+  //事件处理函数
+  gotoPass: function() {
+    wx.navigateTo({
+      url: '../pass/pass'
+    })
+  },
+  onLoad: function () {
+    
+  },
+})
